84bcf8314d7d657f1b7b08a6f4ab64818c27c51d
bartosz0 <methkill@gmail.com>
2021-03-07 22:35:34 +0100

    Added DbContext/Created Init migration and DB

---
+++ RestaurantSeed.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantAPI.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeed
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public RestaurantSeed(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            
+        }
+    }
+}
