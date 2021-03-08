47611ecd194ba8e7ccc815857e188349d00ca23f
bartosz0 <methkill@gmail.com>
2021-03-08 21:21:44 +0100

    Seeded Db with data

--- RestaurantSeed.cs
+++ RestaurantSeed.cs
@@ -20,13 +20,73 @@ namespace RestaurantAPI
                 {
                     var restaurants = GetRestaurants();
                     _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
                 }
             }
         }
 
         private IEnumerable<Restaurant> GetRestaurants()
         {
-            
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description =
+                        "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001",
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "McDonalds",
+                    Category = "Fast Food",
+                    Description =
+                        "McDonald's Corporation is an American fast food company, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States.",
+                    ContactEmail = "contact@mcdonalds.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Big Mac Set",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "McNuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Warszawa",
+                        Street = "Marszałkowska 124",
+                        PostalCode = "00-001",
+                    }
+                }
+            };
+            return restaurants;
         }
     }
 }
