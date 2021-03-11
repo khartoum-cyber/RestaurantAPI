7ae9fe2551b629d09671fca002eb195839cb548c
bartosz0 <methkill@gmail.com>
2021-03-11 21:14:08 +0100

    Changed Address model class

--- Entities/Address.cs
+++ Entities/Address.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RestaurantAPI.Entities
 {
     public class Address
     {
         public int Id { get; set; }
+        [StringLength(50)]
         public string City { get; set; }
+        [StringLength(50)]
         public string Street { get; set; }
         public string PostalCode { get; set; }
         public virtual Restaurant Restaurant { get; set; }
