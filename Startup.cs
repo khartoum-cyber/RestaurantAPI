4a785b8ceff51ba3e19cbfa8bb0040049f268ea5
bartosz0 <methkill@gmail.com>
2021-03-14 22:13:15 +0100

    Refactored controller via separate service

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Services;
 
 namespace RestaurantAPI
 {
@@ -26,6 +27,8 @@ namespace RestaurantAPI
             services.AddScoped<RestaurantSeed>();
 
             services.AddAutoMapper(this.GetType().Assembly);
+
+            services.AddScoped<IRestaurantService, RestaurantService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
