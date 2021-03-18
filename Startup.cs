2b57570c6e59570fd01c61f60bc8ec0bc99b56d3
bartosz0 <methkill@gmail.com>
2021-03-18 12:29:25 +0100

    ErrorHandlingMiddleware

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Middleware;
 using RestaurantAPI.Services;
 
 namespace RestaurantAPI
@@ -29,6 +30,8 @@ namespace RestaurantAPI
             services.AddAutoMapper(this.GetType().Assembly);
 
             services.AddScoped<IRestaurantService, RestaurantService>();
+
+            services.AddScoped<ErrorHandlingMiddleware>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +43,8 @@ namespace RestaurantAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseHttpsRedirection();
 
